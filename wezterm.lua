d97c963aed4a641c9eed46f3c4c6a7fb0e5e535f
meifan <meifan.mf@hotmail.com>
2024-06-30 11:46:30 +0800

    init wezterm config

---
+++ wezterm.lua
@@ -0,0 +1,18 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Everforest Dark Soft (Gogh)'
+config.font = wezterm.font_with_fallback({
+	{ family = "JetBrainsMono Nerd Font Propo", weight = "Bold" },
+	"Noto Color Emoji",
+})
+config.font_size = 14
+
+-- and finally, return the configuration to wezterm
+return config

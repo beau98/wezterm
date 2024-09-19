2497c4eb138038f5b0679e59b564051f3266f468
Mei Fan <codemeifan@gmail.com>
2024-09-19 23:48:13 +0800

    change chinese font

--- wezterm.lua
+++ wezterm.lua
@@ -9,10 +9,12 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = 'Everforest Dark Soft (Gogh)'
 config.font = wezterm.font_with_fallback({
-	{ family = "JetBrainsMono Nerd Font Propo", weight = "Bold" },
-	"Noto Color Emoji",
+ 	{ family = "JetBrainsMono Nerd Font Propo", weight = "Bold" },
+  { family = "苹方-简"},
+  { family = "Noto Color Emoji"}
 })
 config.font_size = 14
+
 
 -- and finally, return the configuration to wezterm
 return config

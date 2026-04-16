7b89ff019b7f71a12d000656a42397c9981c0dff
meifan <meifan.mf@hotmail.com>
2026-04-16 23:13:10 +0800

    optimize front

--- wezterm.lua
+++ wezterm.lua
@@ -1,20 +1,50 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local mux = wezterm.mux
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.color_scheme = 'Everforest Dark Soft (Gogh)'
 config.font = wezterm.font_with_fallback({
- 	{ family = "JetBrainsMono Nerd Font Propo", weight = "Bold" },
-  { family = "苹方-简"},
-  { family = "Noto Color Emoji"}
+  'JetBrains Mono',
+  'PingFang SC',
+  'Apple Color Emoji',
 })
-config.font_size = 14
 
+config.colors = {
+  background = '#1e1f29',
+  foreground = '#d8dee9',
+  cursor_bg = '#c0caf5',
+  cursor_fg = '#1e1f29',
+  selection_bg = '#3b4261',
+  selection_fg = '#c0caf5',
+}
+
+config.font_size = 15.0
+
+-- 更接近 mac 风格的轻提示
+config.freetype_load_target = 'Light'
+
+-- 让显示更舒展一点
+config.line_height = 1.08
+config.cell_width = 1.0
+
+-- 保持默认推荐 shaper
+config.font_shaper = 'Harfbuzz'
+
+
+wezterm.on('gui-startup', function()
+  mux.spawn_window({
+    width = 120,
+    height = 32,
+    position = {
+      x = 680,
+      y = 480,
+      origin = "MainScreen",
+    },
+  })
+end)
 
 -- and finally, return the configuration to wezterm
 return config
+
